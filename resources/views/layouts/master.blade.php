77467fc069073bd0417128297b81a6c7cadf4cfb
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-01-19 10:30:53 +0700

    Add homepage style

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Harun Ar - Rasyid Resume</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    @include('layouts.includes.header')
+</head>
+
+<body>
+    <i class="bi bi-list mobile-nav-toggle d-lg-none"></i>
+    @include('layouts.includes.sidebar')
+
+    @yield('hero-section')
+
+    <main id="main">
+        @yield('content')
+    </main>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    @include('layouts.includes.footer')
+</body>
+
+</html>
